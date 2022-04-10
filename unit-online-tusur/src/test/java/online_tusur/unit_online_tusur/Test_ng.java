3fd2ee0e75c22c59247c328881daaa96b7ff8acd
Sergey Tupikov <Setup21@mail.ru>
2022-04-10 23:51:06 +0700

    add new test

---
+++ unit-online-tusur/src/test/java/online_tusur/unit_online_tusur/Test_ng.java
@@ -0,0 +1,19 @@
+package online_tusur.unit_online_tusur;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+import org.testng.annotations.Test;
+
+public class Test_ng {
+
+	 
+  
+  
+  
+  
+  
+}
